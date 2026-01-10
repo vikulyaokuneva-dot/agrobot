8b0c0cdb3d7dee3d07665959d6245df5781595e9
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-11 00:22:42 +0300

    Delete rss_sources.py

--- rss_sources.py
+++
@@ -1,8 +0,0 @@
-RSS_SOURCES = [
-    "https://rsute.ru/sad-ogorod/feed",
-    "https://www.ogorod.ru/rss",
-    "https://7dach.ru/rss",
-    "https://www.greeninfo.ru/rss.html",
-    "https://www.supersadovnik.ru/rss",
-]
-
