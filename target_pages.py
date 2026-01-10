0615731f7c072f5e46a186f6e0f9d86160a163fe
vikulyaokuneva-dot <vikulyaokuneva@gmail.com>
2026-01-11 00:20:48 +0300

    Create target_pages.py

---
+++ target_pages.py
@@ -0,0 +1,23 @@
+# target_pages.py
+
+# Список URL-адресов, на которых бот будет искать новые статьи.
+# Это могут быть главные страницы разделов, блогов и т.д.
+TARGET_PAGES = [
+    # Supersadovnik: Раздел "Все материалы" отсортирован по новизне
+    'https://www.supersadovnik.ru/posts',
+    
+    # Botanichka: Раздел "Новое на сайте"
+    'https://www.botanichka.ru/blog/new/',
+    
+    # Ogorod.ru: разделы по темам
+    'https://ogorod.ru/ru/ogorod', # Огород
+    'https://ogorod.ru/ru/sad',     # Сад
+    
+    # Dolinasad.by: основной блог
+    'https://dolinasad.by/blog/',
+    
+    # tk-konstruktor.ru: основной блог
+    'https://tk-konstruktor.ru/stati/',
+    
+    # Вы можете добавить больше ссылок сюда
+]
